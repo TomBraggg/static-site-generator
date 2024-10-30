d3c83da1e9bd487012dd7d1d7656951481e13ff7
TomBraggg <tom.bragg.197@gmail.com>
2024-10-30 15:43:54 +0000

    Removed unreachable code

--- mdconvert/parentnode.py
+++ mdconvert/parentnode.py
@@ -11,11 +11,7 @@ class ParentNode(HTMLNode):
             raise ValueError("No tag associated with parent node")
         if not self.children:
             raise ValueError("No children associated with parent node")
-        tree_string = ''
-        if type(self) == LeafNode:
-            tree_string += self.to_html()
-            return tree_string
-        tree_string += f'<{self.tag}{self.props_to_html()}>' 
+        tree_string = f'<{self.tag}{self.props_to_html()}>' 
         for child in self.children:
             tree_string += child.to_html()
         tree_string += f'</{self.tag}>'
